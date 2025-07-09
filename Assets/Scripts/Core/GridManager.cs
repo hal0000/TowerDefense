91e959790c801a43c1dcf3b8562e9c83cac11658
halilmentes <halilmentes96@gmail.com>
2025-07-09 23:24:56 +0200

    grid generation added

---
+++ Assets/Scripts/Core/GridManager.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TowerDefense.Model;
+
+namespace TowerDefense.Core
+{
+
+    [RequireComponent(typeof(Transform))]
+    public class GridManager : MonoBehaviour
+    {
+        [Header("Grid Settings")]
+        [Tooltip("Prefab that contains a CellView component")]
+        [SerializeField] private CellView CellPrefab;
+
+        private CellView[] _cells;
+        private int _gridWidth, _gridHeight;
+
+        /// <summary>
+        /// A simple random‐axis walk from (0,0) → (w-1,h-1).
+        /// </summary>
+        private List<int> ComputePath(int w, int h)
+        {
+            var path = new List<int>();
+            int x = 0, y = 0, tx = w - 1, ty = h - 1;
+            path.Add(CoordPacker.Pack(x, y));
+            while (x != tx || y != ty)
+            {
+                bool canX = x != tx, canY = y != ty;
+                if (canX && (!canY || Random.value < 0.5f)) x += tx > x ? 1 : -1;
+                else y += ty > y ? 1 : -1;
+                path.Add(CoordPacker.Pack(x, y));
+            }
+            return path;
+        }
+        
+        public void BuildGrid(int w, int h)
+        {
+            // if we already built once just reset fields
+            if (_cells != null && _cells.Length > 0)
+            {
+                ResetGrid();
+                return;
+            }
+            _gridWidth = w;
+            _gridHeight = h;
+            _cells = new CellView[w * h];
+
+            var pathSet = new HashSet<int>(ComputePath(w, h));
+            Quaternion rot = CellPrefab.gameObject.transform.rotation;
+
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    int packed = CoordPacker.Pack(x, y);
+                    int index = y * w + x; // flat‐array index
+
+                    var go = Instantiate(CellPrefab, new Vector3(x, 0f, y), rot, transform);
+#if UNITY_EDITOR
+                    go.name = $"Cell_{x}_{y}";
+#endif
+                    var cm = new CellModel(go.name, x, y);
+                    if (pathSet.Contains(packed)) cm.SetPath(true);
+                    go.Initialize(cm);
+                    _cells[index] = go;
+                }
+            }
+        }
+        private void ResetGrid()
+        {
+            for (int i = 0, len = _cells.Length; i < len; i++)
+            {
+                var cv = _cells[i];
+                cv.Model.SetOccupied(false);
+                cv.Model.SetPath(false);
+                cv.ResetHighlight();
+            }
+        }
+        private bool TryGetIndex(int x, int y, out int index)
+        {
+            if (x < 0 || x >= _gridWidth || y < 0 || y >= _gridHeight)
+            {
+                index = -1;
+                return false;
+            }
+            index = y * _gridWidth + x;
+            return true;
+        }
+
+        public bool WorldToPackedCoord(Vector3 worldPos, out int packed)
+        {
+            int x = Mathf.FloorToInt(worldPos.x);
+            int y = Mathf.FloorToInt(worldPos.z);
+            packed = CoordPacker.Pack(x, y);
+            return TryGetIndex(x, y, out _);
+        }
+
+        public CellView GetCellView(int packed)
+        {
+            int x = CoordPacker.UnpackX(packed);
+            int y = CoordPacker.UnpackY(packed);
+            return TryGetIndex(x, y, out int idx) ? _cells[idx] : null;
+        }
+        
+        public Vector3 GetCellCenter(int packed)
+        {
+            int x = CoordPacker.UnpackX(packed), y = CoordPacker.UnpackY(packed);
+            return new Vector3(x + .5f, 0f, y + .5f);
+        }
+    }
+}
